1385b68079e31f1caf40e1fc6642cd74cf546dff
Samuel Pontes <pontes.samuel@outlook.com>
2023-10-30 19:52:04 -0300

    addaulas

--- aula48.py
+++ aula48.py
@@ -2,13 +2,16 @@
 split e join com list e str
 split - divide uma string (list)
 join - une uma string
+strip corta os espaços da esquerda e direita
+lstrip corta os espaços da esquerda
+rstrip corta os espaços da direita
 """
 frase = '   Olha só que   , coisa interessante          '
 lista_frases_cruas = frase.split(',')
 
 lista_frases = []
 for i, frase in enumerate(lista_frases_cruas):
-    lista_frases.append(lista_frases_cruas[i].strip())
+    lista_frases.append(lista_frases_cruas[i].strip()) #strip corta os espaços da esquerda e direita   
 
 # print(lista_frases_cruas)
 # print(lista_frases)
