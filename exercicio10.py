4356512dcc5048bada2a761fd2aa3d079d9bf559
Samuel Pontes <pontes.samuel@outlook.com>
2024-02-25 14:32:47 -0300

    exerciciosCPF

--- exercicio10.py
+++ exercicio10.py
@@ -23,15 +23,61 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
+
+"""
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O segundo dígito do CPF é 0
+"""
+
 cpf = '74682489070'
 nove_digitos = cpf [:9]
 contador1 = 10
 resultado1 = 0
 
-for digito in nove_digitos:
-    resultado1 += (int(digito) * contador1)
+for digitos in nove_digitos:
+    resultado1 += (int(digitos) * contador1)
     contador1 -= 1
+    
 primeiro_digito = (resultado1 * 10 % 11)
 primeiro_digito = primeiro_digito if primeiro_digito <= 9 else 0
-print(primeiro_digito)
-    
+
+dez_digitos = nove_digitos + str(primeiro_digito)
+contador2 = 10
+resultado2 = 0
+
+resultado2 = 0
+for digito in dez_digitos:
+    resultado2 += (int(digitos)*contador2)
+    contador2 -= 1
+
+segundo_digito = (resultado2 * 10) % 11
+segundo_digito = segundo_digito if segundo_digito <=9 else 0
+
+cpfvalidado = f'{nove_digitos}{primeiro_digito}{segundo_digito}'
+
+if cpf == cpfvalidado:
+    print (f'CPF {cpfvalidado} é valido ')
+else:
+    print ('CPF invalido')
