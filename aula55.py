c44ee24e1a8f647303c1e3a9e13ebb5f37e275a4
Samuel Pontes <pontes.samuel@outlook.com>
2024-02-29 18:47:15 -0300

    addaula

--- aula55.py
+++ aula55.py
@@ -6,11 +6,11 @@ Argumento não nomeado recebe apenas o argumento (valor)
 
 
 def soma(x, y, z):
-    # Definição
+    # Definição parametro é a variavel
     print(f'{x=} y={y} {z=}', '|', 'x + y + z = ', x + y + z)
 
 
-soma(1, 2, 3)
-soma(1, y=2, z=5) #argumento nomeado
+soma(1, 2, 3) #argumento nao nomeado
+soma(1, y=2, z=5) #argumento nomeado se normear um os proximos precisam ser nomeados tambem
 
 print(1, 2, 3, sep='-')
